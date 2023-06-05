e30043b185b47aa309a68a48bab3deac4db65fe5
Diego <diego.taka@hotmail.com>
2023-06-05 15:30:57 -0300

    Camada de serviço, componente spring e endpoint findById

--- src/main/java/com/desafio/cadastro/resources/PessoaResource.java
+++ src/main/java/com/desafio/cadastro/resources/PessoaResource.java
@@ -1,13 +1,17 @@
 package com.desafio.cadastro.resources;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafio.cadastro.entities.Pessoa;
+import com.desafio.cadastro.services.PessoaService;
 
 
 
@@ -15,10 +19,19 @@ import com.desafio.cadastro.entities.Pessoa;
 @RequestMapping(value = "/pessoas")
 public class PessoaResource {
 	
+	@Autowired
+	private PessoaService service;
+	
 	@GetMapping
-	public ResponseEntity<Pessoa> findAll() {
-		Pessoa p = new Pessoa(1L, "Diego", "12360043851", LocalDate.of(1999, 7, 2));
-		return ResponseEntity.ok().body(p);
-		
+	public ResponseEntity<List<Pessoa>> findAll() {
+		List<Pessoa> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Pessoa> findById(@PathVariable Long id){
+		Pessoa obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
---
+++ src/main/java/com/desafio/cadastro/services/PessoaService.java
@@ -0,0 +1,27 @@
+package com.desafio.cadastro.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.desafio.cadastro.entities.Pessoa;
+import com.desafio.cadastro.repositories.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository repository;
+	
+	public List<Pessoa> findAll() {
+		return repository.findAll();
+	}
+	
+	public Pessoa findById(Long id) {
+		Optional<Pessoa> obj = repository.findById(id);
+		return obj.get();
+	}
+}
+
