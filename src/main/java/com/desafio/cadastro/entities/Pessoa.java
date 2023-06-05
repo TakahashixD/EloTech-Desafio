1664610969f0aa77b6f202b18e242fd743bc5940
Diego <diego.taka@hotmail.com>
2023-06-05 11:53:51 -0300

    Entidade Pessoa e recurso

---
+++ src/main/java/com/desafio/cadastro/entities/Pessoa.java
@@ -0,0 +1,83 @@
+package com.desafio.cadastro.entities;
+
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataNascimento;
+	//private List<Contatos> contatos = new ArrayList<>();
+	
+	//Constructors
+	public Pessoa() {
+	}
+	public Pessoa(Long id, String name, String cpf, LocalDate dataNascimento) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	//Getters e Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	//hashCode e equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/desafio/cadastro/resources/PessoaResource.java
@@ -0,0 +1,24 @@
+package com.desafio.cadastro.resources;
+
+import java.time.LocalDate;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.cadastro.entities.Pessoa;
+
+
+
+@RestController
+@RequestMapping(value = "/pessoas")
+public class PessoaResource {
+	
+	@GetMapping
+	public ResponseEntity<Pessoa> findAll() {
+		Pessoa p = new Pessoa(1L, "Diego", "43160014851", LocalDate.of(1999, 7, 2));
+		return ResponseEntity.ok().body(p);
+		
+	}
+}
