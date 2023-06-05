202b84da6a3be94139f8fe0d640cd8b89482843b
Diego <diego.taka@hotmail.com>
2023-06-05 12:32:49 -0300

    H2, test e JPA

--- src/main/java/com/desafio/cadastro/entities/Pessoa.java
+++ src/main/java/com/desafio/cadastro/entities/Pessoa.java
@@ -3,16 +3,22 @@ package com.desafio.cadastro.entities;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pessoa implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
