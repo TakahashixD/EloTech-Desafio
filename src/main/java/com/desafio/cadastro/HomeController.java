17df13cabcbd7d6241feed132a272f77c996c288
Diego <diego.taka@hotmail.com>
2023-06-07 14:51:05 -0300

    menssagem de aplicação rodando (home)

---
+++ src/main/java/com/desafio/cadastro/HomeController.java
@@ -0,0 +1,12 @@
+package com.desafio.cadastro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping({"/", "/home", "/status"})
+	public String getStatus() {
+		return "A aplicação está funcionando";
+	}
+}
